2eb847f3613ce875d8d3578b0ea4c5bed898d8ca
Michael Aaron Murphy <mmstickman@gmail.com>
2017-01-18 15:10:54 -0500

    Implement Jobs +/- Modifiers You may now write the job arguments with + and - operators - +50% is equivalent to 150% - -50% is equivalent to 50% - +2 is equivalent to num_cpus + 2 - -2 is equivalent to num_cpus - 2 - If the negative modifier is less than number of CPU cores, set 1

--- src/arguments/jobs.rs
+++ src/arguments/jobs.rs
@@ -5,22 +5,57 @@ use num_cpus;
 /// be calculated as a percent of the total number of CPU cores. Otherwise, the number provided
 /// will be considered the number of jobs to run in parallel.
 pub fn parse(value: &str) -> Result<usize, ParseErr> {
-    if value.chars().rev().next().unwrap() == '%' {
-        // If the last character is `%`, then all but the last character are the value.
-        value[0..value.chars().count()-1].parse::<usize>()
-            .map(|percent| (num_cpus::get() * percent) / 100)
+    match (value.bytes().next().unwrap(), value.bytes().last().unwrap()) {
+        (b'+', b'%') => {
+            value[1..value.bytes().count()-1].parse::<usize>()
+            .map(|percent| {
+                let ncpus = num_cpus::get();
+                ncpus + ((ncpus * percent) / 100)
+            })
             .map_err(|_| ParseErr::JobsNaN(value.to_owned()))
-
-    } else {
-        value.parse::<usize>().map_err(|_| ParseErr::JobsNaN(value.to_owned()))
+        },
+        (b'-', b'%') => {
+            value[1..value.bytes().count()-1].parse::<usize>()
+                .map(|percent| {
+                    let ncpus = num_cpus::get();
+                    let modifier = (ncpus * percent) / 100;
+                    if modifier > ncpus { 1 } else { ncpus - modifier }
+                })
+                .map_err(|_| ParseErr::JobsNaN(value.to_owned()))
+        },
+        (_, b'%') => {
+            value[0..value.bytes().count()-1].parse::<usize>()
+                .map(|percent| (num_cpus::get() * percent) / 100)
+                .map_err(|_| ParseErr::JobsNaN(value.to_owned()))
+        },
+        (b'+', _) => {
+            value[1..value.bytes().count()].parse::<usize>()
+                .map(|value| num_cpus::get() + value)
+                .map_err(|_| ParseErr::JobsNaN(value.to_owned()))
+        },
+        (b'-', _) => {
+            value[1..value.bytes().count()].parse::<usize>()
+            .map(|value| {
+                let ncpus = num_cpus::get();
+                if value > ncpus { 1 } else { ncpus - value }
+            })
+            .map_err(|_| ParseErr::JobsNaN(value.to_owned()))
+        },
+        _ => {
+            value.parse::<usize>().map_err(|_| ParseErr::JobsNaN(value.to_owned()))
+        },
     }
 }
 
 #[test]
-fn test_parse() {
+fn job_parsing() {
     let ncores = num_cpus::get();
     assert_eq!((ncores * 50) / 100,  parse("50%" ).unwrap());
     assert_eq!((ncores * 100) / 100, parse("100%").unwrap());
     assert_eq!((ncores * 150) / 100, parse("150%").unwrap());
     assert_eq!(4,                    parse("4"   ).unwrap());
+    assert_eq!((ncores * 150) / 100, parse("+50%").unwrap());
+    assert_eq!((ncores * 50) / 100,  parse("-50%").unwrap());
+    assert_eq!(ncores - 2,           parse("-2"  ).unwrap());
+    assert_eq!(ncores + 2,           parse("+2"  ).unwrap());
 }
